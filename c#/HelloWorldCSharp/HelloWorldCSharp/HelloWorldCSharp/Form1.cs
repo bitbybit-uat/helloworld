a001084e701e6ef09d1a6d6877da821a296e2ee4
MICAH TURPIN <MICTURPI@uat.edu>
2022-07-07 19:40:56 -0700

    Added C# directory and helloworld example

---
+++ c#/HelloWorldCSharp/HelloWorldCSharp/HelloWorldCSharp/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HelloWorldCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Console.WriteLine("Hello World!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Hello World!";
+        }
+    }
+}
